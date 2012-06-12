863f6d1ce7d71b0c00538df1694ef8e4d56d587e
Andrew Capshaw <andrew.capshaw@gmail.com>
2012-06-11 23:19:58 -0500

    Added initial files. Nothing too interesting :)

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-andrew-capshaw-com
+AndrewCapshaw.com
 ==================
 
 The content of my personal website. I figured I'd make it open source :)
+
+For now this is purely a static site. I had previously to this point utilized a content management system that I created a few years back (affectionately called Citrus CMS) which allowed me to post dynamic content. Since I have grown out of that code and needed a clean slate, I decided to start with this, a site with no dynamics. Over the next few months, I hope to grow it into a new and dynamic site. 
---
+++ index.php
@@ -0,0 +1,86 @@
+<!-- Hello you! Glad you're inspecting the code :) -->
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset='utf-8' />
+	<title>Andrew Capshaw | Portfolio</title>
+
+	<!-- Google Web Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+	<link rel='stylesheet' type='text/css' href='css/style.css' />
+	<link rel='shortcut icon' type='image/x-icon' href='img/common/foreach.ico' />
+</head>
+
+<body id='home'>
+
+	<div id='nav'>
+		<div class='container'>
+			<div class='forall'>
+				&forall;
+			</div>
+			<a href='#' class='navlink'>
+				about
+			</a>
+			&middot;
+			<a href='#' class='navlink'>
+				portfolio
+			</a>
+			&middot;
+			<a href='#' class='navlink'>
+				resume
+			</a>
+			&middot; 
+			<a href='#' class='navlink'>
+				contact
+			</a> 
+		</div>
+	</div>
+
+	<div id='maincontainer' class='container'>
+
+		<div id='about' class='flowleft'>
+			<div class='welcome'>
+				Hello!
+			</div>
+			<p>I'm Andrew Capshaw. I'm an undergraduate student studying computer science
+			 at <a href='#'>Rice University</a> in Houston, TX.</p>
+			 <p>This is my personal site &amp; portfolio. Have a look around and feel free
+			 	to contact me for any reason!</p>
+		</div>
+
+		<div id='main' class='flowright'>
+			<div class='main-content'>
+				<div class='heading'>
+					<h2>about me</h2>
+				</div>
+				<p>
+									
+				</p>
+				<div class='heading'>
+					<h2>portfolio</h2>
+				</div>
+				<p>
+				Praesent dignissim erat id dui tristique imperdiet fringilla vel mauris. Morbi tortor sapien, commodo sed consectetur ac, eleifend non dolor. Vivamus ullamcorper interdum posuere. Sed bibendum viverra tortor, vitae semper mi iaculis et. Curabitur nulla tortor, cursus sed consectetur non, blandit sit amet eros. Duis congue, massa vel facilisis fermentum, nunc diam faucibus ipsum, pretium rutrum metus justo a est. Suspendisse tincidunt malesuada arcu sit amet varius. Pellentesque vel nisi nisi. Proin volutpat metus vitae diam dapibus a venenatis felis cursus. Maecenas lectus orci, malesuada feugiat interdum non, sollicitudin sit amet lorem. Suspendisse vehicula placerat dolor quis pretium. Pellentesque tristique malesuada erat, sit amet imperdiet justo aliquam sit amet. Quisque erat diam, condimentum eu aliquam ut, elementum eget turpis. Nam volutpat tellus ut orci faucibus posuere ut quis massa. Mauris egestas, augue vel tempor vestibulum, est lorem tempor mi, vel volutpat dui odio eu nunc. Proin ullamcorper augue quis purus rutrum blandit.
+				</p>
+				<div class='heading'>
+					<h2>resume</h2>
+				</div>
+				<p>
+					This website is intended as a resume of sorts, but if you'd like a printable / traditional pdf, I've got something here just for you!
+				</p>
+				<p>
+				Proin a enim non ipsum porta elementum. Aliquam sollicitudin eros vitae nisi lacinia pharetra. Cras in erat at odio mollis imperdiet. Proin sodales ipsum sed lacus malesuada at lobortis ante malesuada. Aliquam vestibulum facilisis erat. Donec quis tristique augue. Quisque ut turpis nibh. Ut tincidunt rutrum ligula, ac consectetur felis pretium sed. Phasellus vitae est quis metus porttitor vestibulum eu in est. Integer pellentesque massa eget leo mattis dictum fringilla ante ullamcorper. Sed sem nulla, ultrices ut pharetra non, iaculis et leo.
+				</p>
+				<p>
+				Maecenas vel augue nec est blandit tempus id at diam. Morbi volutpat tincidunt dui. Vivamus et leo justo. Phasellus suscipit nunc aliquam arcu imperdiet sit amet interdum ligula lacinia. Maecenas in metus justo. Nunc volutpat placerat pulvinar. Pellentesque mauris massa, luctus vel faucibus a, condimentum in purus. Integer lobortis blandit ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</p>
+			</div>
+		</div>
+
+		<div class='clearboth'><!-- --></div>
+
+	</div>
+
+</body>
+</html>
