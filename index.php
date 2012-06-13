d590e4b76f9860bb3adfd280ffc3b048b3243f6a
Andrew Capshaw <andrew.capshaw@gmail.com>
2012-06-12 21:58:21 -0500

    Added a psudeo-polaroid style and played around with link styling.

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 			<div class='forall'>
 				&forall;
 			</div>
-			<a href='#' class='navlink'>
+			<a href='#' class='navlink selected-link'>
 				about
 			</a>
 			&middot;
@@ -40,42 +40,58 @@
 	<div id='maincontainer' class='container'>
 
 		<div id='about' class='flowleft'>
-			<div class='welcome'>
-				Hello!
+			<div class='outer-shell'>
+				<div class='inner-shell me'>
+					<!-- -->
+				</div>
 			</div>
-			<p>I'm Andrew Capshaw. I'm an undergraduate student studying computer science
-			 at <a href='#'>Rice University</a> in Houston, TX.</p>
-			 <p>This is my personal site &amp; portfolio. Have a look around and feel free
-			 	to contact me for any reason!</p>
+			<div class='main-content'>
+				<p>Hello, I'm Andrew!</p>
+
+				<p>
+					I'm an undergraduate student studying computer science at <a href='#'>Rice University</a> in Houston, TX.
+				</p>
+				<p>
+					This is my personal site &amp; portfolio. Have a look around and feel free to contact me for any reason!
+				</p>
+				<p>
+					Email: <span>capshaw@</span>rice.edu
+			</div>
 		</div>
 
 		<div id='main' class='flowright'>
-			<div class='main-content'>
-				<div class='heading'>
-					<h2>about me</h2>
+			<div class='outer-shell'>
+				<div class='inner-shell portfolio'>
+					<!-- -->
 				</div>
+			</div>
+			<div class='main-content'>
+				<h2>Who am I?</h2>
 				<p>
 					Test. Well this is embarrassing. Somehow you've found a site that I'm currently in the process of developing.
 					Feel free to click around but just know that this is still a work in progress.		
 				</p>
-				<div class='heading'>
-					<h2>portfolio</h2>
-				</div>
 				<p>
 				Praesent dignissim erat id dui tristique imperdiet fringilla vel mauris. Morbi tortor sapien, commodo sed consectetur ac, eleifend non dolor. Vivamus ullamcorper interdum posuere. Sed bibendum viverra tortor, vitae semper mi iaculis et. Curabitur nulla tortor, cursus sed consectetur non, blandit sit amet eros. Duis congue, massa vel facilisis fermentum, nunc diam faucibus ipsum, pretium rutrum metus justo a est. Suspendisse tincidunt malesuada arcu sit amet varius. Pellentesque vel nisi nisi. Proin volutpat metus vitae diam dapibus a venenatis felis cursus. Maecenas lectus orci, malesuada feugiat interdum non, sollicitudin sit amet lorem. Suspendisse vehicula placerat dolor quis pretium. Pellentesque tristique malesuada erat, sit amet imperdiet justo aliquam sit amet. Quisque erat diam, condimentum eu aliquam ut, elementum eget turpis. Nam volutpat tellus ut orci faucibus posuere ut quis massa. Mauris egestas, augue vel tempor vestibulum, est lorem tempor mi, vel volutpat dui odio eu nunc. Proin ullamcorper augue quis purus rutrum blandit.
 				</p>
-				<div class='heading'>
-					<h2>resume</h2>
+				<h2>Projects</h2>
+				<div class='inner-shell portfolio'>
+					<!-- -->
 				</div>
 				<p>
 				Proin a enim non ipsum porta elementum. Aliquam sollicitudin eros vitae nisi lacinia pharetra. Cras in erat at odio mollis imperdiet. Proin sodales ipsum sed lacus malesuada at lobortis ante malesuada. Aliquam vestibulum facilisis erat. Donec quis tristique augue. Quisque ut turpis nibh. Ut tincidunt rutrum ligula, ac consectetur felis pretium sed. Phasellus vitae est quis metus porttitor vestibulum eu in est. Integer pellentesque massa eget leo mattis dictum fringilla ante ullamcorper. Sed sem nulla, ultrices ut pharetra non, iaculis et leo.
 				</p>
+				<div class='inner-shell portfolio'>
+					<!-- -->
+				</div>
 				<p>
 				Maecenas vel augue nec est blandit tempus id at diam. Morbi volutpat tincidunt dui. Vivamus et leo justo. Phasellus suscipit nunc aliquam arcu imperdiet sit amet interdum ligula lacinia. Maecenas in metus justo. Nunc volutpat placerat pulvinar. Pellentesque mauris massa, luctus vel faucibus a, condimentum in purus. Integer lobortis blandit ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 				</p>
+				<h2>Resume</h2>
 				<p>
 				Proin a enim non ipsum porta elementum. Aliquam sollicitudin eros vitae nisi lacinia pharetra. Cras in erat at odio mollis imperdiet. Proin sodales ipsum sed lacus malesuada at lobortis ante malesuada. Aliquam vestibulum facilisis erat. Donec quis tristique augue. Quisque ut turpis nibh. Ut tincidunt rutrum ligula, ac consectetur felis pretium sed. Phasellus vitae est quis metus porttitor vestibulum eu in est. Integer pellentesque massa eget leo mattis dictum fringilla ante ullamcorper. Sed sem nulla, ultrices ut pharetra non, iaculis et leo.
 				</p>
+				<h2>Contact Me</h2>
 				<p>
 				Maecenas vel augue nec est blandit tempus id at diam. Morbi volutpat tincidunt dui. Vivamus et leo justo. Phasellus suscipit nunc aliquam arcu imperdiet sit amet interdum ligula lacinia. Maecenas in metus justo. Nunc volutpat placerat pulvinar. Pellentesque mauris massa, luctus vel faucibus a, condimentum in purus. Integer lobortis blandit ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 				</p>
