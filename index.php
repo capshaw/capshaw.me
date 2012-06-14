cd0ae41fb7cdd4603f50a3db521dfac3105296c0
Andrew Capshaw <andrew.capshaw@gmail.com>
2012-06-13 23:29:52 -0500

    Still working on initial site.

--- index.php
+++ index.php
@@ -13,6 +13,13 @@
 
 	<script src="js/jquery-1.7.2.min.js"></script>
 	<script src="js/jquery-ui-1.8.21.custom.min.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+
+	<!-- Mixpanel Analytics -->
+	<!--script type="text/javascript">(function(d,c){var a,b,g,e;a=d.createElement("script");a.type="text/javascript";a.async=!0;a.src=("https:"===d.location.protocol?"https:":"http:")+'//api.mixpanel.com/site_media/js/api/mixpanel.2.js';b=d.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b);c._i=[];c.init=function(a,d,f){var b=c;"undefined"!==typeof f?b=c[f]=[]:f="mixpanel";g="disable track track_pageview track_links track_forms register register_once unregister identify name_tag set_config".split(" ");
+		for(e=0;e<g.length;e++)(function(a){b[a]=function(){b.push([a].concat(Array.prototype.slice.call(arguments,0)))}})(g[e]);c._i.push([a,d,f])};window.mixpanel=c})(document,[]);
+		mixpanel.init("119d3215461bea63d91687fe535a53c9");
+	</script-->
 
 </head>
 
@@ -20,100 +27,147 @@
 
 	<div id='nav'>
 		<div class='container text-right'>
-			<div class='forall flowleft'>
+			<a id='for-all' class='flowleft scroller' href='#home' onClick='mixpanel.track("ForAll Link");'>
 				&forall;
+			</a>
+			<div class='flowleft five-px-margin-lr no-opacity' id='to-the-top'>
+				to the top
 			</div>
-			<a href='#' id='aboutLink' class='navlink selected-link'>
+			<a href='#about' id='aboutLink' class='nav-link scroller selected-link' onClick='mixpanel.track("About Link");'>
 				about
 			</a>
 			&middot;
-			<a href='#' class='navlink'>
+			<a href='#portfolio' class='nav-link scroller' onClick='mixpanel.track("Portfolio Link");'>
 				portfolio
 			</a>
 			&middot;
-			<a href='#' class='navlink'>
+			<a href='#' class='nav-link scroller' onClick='mixpanel.track("Resume Link");'>
 				resume
 			</a>
 			&middot; 
-			<a href='#aboutme' id='contactLink' class='navlink'>
+			<a href='#' id='contactLink' class='nav-link scroller' onClick='mixpanel.track("Contact Link");'>
 				contact
 			</a> 
 		</div>
 	</div>
 
+
 	<div id='maincontainer' class='container'>
 
-		<div id='about' class='flowleft'>
-			<div class='outer-shell'>
-				<div class='inner-shell me'>
-					<!-- -->
+		<div class='section' id='about'>
+			<div class='side-bar flowleft'>
+				<div class='outer-shell'>
+					<div class='inner-shell me'>
+						<!-- -->
+					</div>
 				</div>
 			</div>
-			<div class='main-content' id='aboutme'>
-				<p>Hello, I'm Andrew!</p>
 
-				<p>
-					I'm an undergraduate studying computer science at <a href='#'>Rice University</a> in Houston, TX.
-				</p>
-				<p>
-					This is my personal site &amp; portfolio. Have a look around and feel free to contact me for any reason!
-				</p>
-				<p id='contact' class='quote'>
-					Email: <a href='mailto:capshaw@rice.edu'><span>capshaw@</span>rice.edu</a><br>
-					Twitter: <a href='http://twitter.com/#!/thecapshaw'>@thecapshaw</a>
-				</p>
+			<div class='main flowright'>
+				<div class='main-content'>
+					<h2>Who am I?</h2>
+					<p>Hello, I'm Andrew. I'm an undergraduate studying computer science at <a href='#'>Rice University</a> in Houston, TX.
+					</p>
+					<p>
+						This is my personal site &amp; portfolio. Have a look around and feel free to contact me for any reason!
+					</p>
+				</div>
 			</div>
-		</div>
 
-		<div id='main' class='flowright'>
-			<div class='outer-shell'>
-				<div class='inner-shell portfolio'>
-					<!-- -->
-				</div>
-			</div>
-			<div class='main-content'>
-				<h2>Who am I?</h2>
-				<p>
-					Test. Well this is embarrassing. Somehow you've found a site that I'm currently in the process of developing.
-					Feel free to click around but just know that this is still a work in progress.		
-				</p>
-				<p>
-				Praesent dignissim erat id dui tristique imperdiet fringilla vel mauris. Morbi tortor sapien, commodo sed consectetur ac, eleifend non dolor. Vivamus ullamcorper interdum posuere. Sed bibendum viverra tortor, vitae semper mi iaculis et. Curabitur nulla tortor, cursus sed consectetur non, blandit sit amet eros. Duis congue, massa vel facilisis fermentum, nunc diam faucibus ipsum, pretium rutrum metus justo a est. Suspendisse tincidunt malesuada arcu sit amet varius. Pellentesque vel nisi nisi. Proin volutpat metus vitae diam dapibus a venenatis felis cursus. Maecenas lectus orci, malesuada feugiat interdum non, sollicitudin sit amet lorem. Suspendisse vehicula placerat dolor quis pretium. Pellentesque tristique malesuada erat, sit amet imperdiet justo aliquam sit amet. Quisque erat diam, condimentum eu aliquam ut, elementum eget turpis. Nam volutpat tellus ut orci faucibus posuere ut quis massa. Mauris egestas, augue vel tempor vestibulum, est lorem tempor mi, vel volutpat dui odio eu nunc. Proin ullamcorper augue quis purus rutrum blandit.
-				</p>
-				<h2>Projects</h2>
-				<div class='inner-shell portfolio'>
-					<!-- -->
-				</div>
-				<p>
-				Proin a enim non ipsum porta elementum. Aliquam sollicitudin eros vitae nisi lacinia pharetra. Cras in erat at odio mollis imperdiet. Proin sodales ipsum sed lacus malesuada at lobortis ante malesuada. Aliquam vestibulum facilisis erat. Donec quis tristique augue. Quisque ut turpis nibh. Ut tincidunt rutrum ligula, ac consectetur felis pretium sed. Phasellus vitae est quis metus porttitor vestibulum eu in est. Integer pellentesque massa eget leo mattis dictum fringilla ante ullamcorper. Sed sem nulla, ultrices ut pharetra non, iaculis et leo.
-				</p>
-				<div class='inner-shell portfolio'>
-					<!-- -->
-				</div>
-				<p>
-				Maecenas vel augue nec est blandit tempus id at diam. Morbi volutpat tincidunt dui. Vivamus et leo justo. Phasellus suscipit nunc aliquam arcu imperdiet sit amet interdum ligula lacinia. Maecenas in metus justo. Nunc volutpat placerat pulvinar. Pellentesque mauris massa, luctus vel faucibus a, condimentum in purus. Integer lobortis blandit ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				</p>
-				<h2>Resume</h2>
-				<p>
-				Proin a enim non ipsum porta elementum. Aliquam sollicitudin eros vitae nisi lacinia pharetra. Cras in erat at odio mollis imperdiet. Proin sodales ipsum sed lacus malesuada at lobortis ante malesuada. Aliquam vestibulum facilisis erat. Donec quis tristique augue. Quisque ut turpis nibh. Ut tincidunt rutrum ligula, ac consectetur felis pretium sed. Phasellus vitae est quis metus porttitor vestibulum eu in est. Integer pellentesque massa eget leo mattis dictum fringilla ante ullamcorper. Sed sem nulla, ultrices ut pharetra non, iaculis et leo.
-				</p>
-				<h2>Contact Me</h2>
-				<p>
-				Maecenas vel augue nec est blandit tempus id at diam. Morbi volutpat tincidunt dui. Vivamus et leo justo. Phasellus suscipit nunc aliquam arcu imperdiet sit amet interdum ligula lacinia. Maecenas in metus justo. Nunc volutpat placerat pulvinar. Pellentesque mauris massa, luctus vel faucibus a, condimentum in purus. Integer lobortis blandit ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				</p>
+			<div class='clear-both'>
+				<!-- -->
 			</div>
 		</div>
 
-		<div class='clearboth'><!-- --></div>
 
+	<div class='section' id='portfolio'>
+		<div class='clear-both p-top-20'>
+			<div id='side-bar' class='side-bar flowleft'>
+				<h2>Alice&amp;Love</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet euismod sem, gravida <a href='#'>pharetra mauris</a> sollicitudin at. Vivamus ac viverra lacus. Curabitur a leo est.
+				</p>
+			</div>
+			<div class='main flowright'>
+				<div class='outer-shell'>
+					<div class='inner-shell portfolio' style='background:#eee url("http://placehold.it/660x300&text=Portfolio+Image") center center;'>
+						<!-- -->
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class='clear-both p-top-20'>
+			<div id='side-bar' class='side-bar flowleft'>
+				<h2>Citrus Content Management System</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet euismod sem, gravida <a href='#'>pharetra mauris</a> sollicitudin at. Vivamus ac viverra lacus. Curabitur a leo est.
+				</p>
+			</div>
+			<div class='main flowright'>
+				<div class='outer-shell'>
+					<div class='inner-shell portfolio' style='background:#eee url("http://placehold.it/660x300&text=Portfolio+Image") center center;'>
+						<!-- -->
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class='clear-both p-top-20'>
+			<div id='side-bar' class='side-bar flowleft'>
+				<h2>The Greedy Painters</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet euismod sem, gravida <a href='#'>pharetra mauris</a> sollicitudin at. Vivamus ac viverra lacus. Curabitur a leo est.
+				</p>
+			</div>
+			<div class='main flowright'>
+				<div class='outer-shell'>
+					<div class='inner-shell portfolio' style='background:#eee url("http://placehold.it/660x300&text=Portfolio+Image") center center;'>
+						<!-- -->
+					</div>
+				</div>
+			</div>
+
+			<div class='clear-both'>
+				<!-- -->
+			</div>
+		</div>
 	</div>
 
+
+	<script type='text/javascript'>
+		$("#contactLink").click(function () {
+			$("#contact").switchClass('quote','highlighted',500,'easeOutBounce',function(){
+			  // console.log('transition is done!');
+			});
+	    });
+
+	    $("#for-all").hover(
+	    	function () {
+		    	$("#to-the-top").switchClass('no-opacity', 'full-opacity', 1000, 'easeInSine', null);
+		    }, 
+		    function () {
+		    	$("#to-the-top").switchClass('full-opacity', 'no-opacity', 1000, 'easeInSine', null);
+	    	}
+	    );
+
+		/* Binds the nav-link class to the jquery scrolling functionality. */
+	    $('.scroller').bind('click', function(event){
+
+	        var $anchor = $(this);
+	 
+	        $('html, body').stop().animate({
+	            scrollTop: $($anchor.attr('href')).offset().top
+	        }, 1500,'easeInOutExpo');
+
+	        event.preventDefault();
+	    });
+	</script>
+						<!-- 		<p id='contact'>
+						<span class='tag'>Email</span>
+						<a href='mailto:capshaw@rice.edu'><span>capshaw@</span>rice.edu</a><br>
+						<span class='tag'>Twitter</span> 
+						<a href='http://twitter.com/#!/thecapshaw'>@thecapshaw</a>
+					</p> -->
 </body>
-<script type='text/javascript'>
-	$("#contactLink").click(function () {
-		$("#contact").switchClass('quote','highlighted',500,'easeOutBounce',function(){
-		  console.log('transition is done!');
-		});
-    });
-</script>
 </html>
