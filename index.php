cb46ef1e96f402be38d6b80c8708fff9dfe444a1
Andrew Capshaw <andrew.capshaw@gmail.com>
2012-10-11 10:24:50 -0500

    Switched google analytics to gosquared

--- index.php
+++ index.php
@@ -25,19 +25,6 @@
 	<script type='text/javascript' src="js/jquery-1.7.2.min.js"></script>
 	<script type='text/javascript' src="js/jquery-ui-1.8.21.custom.min.js"></script>
 	<script type='text/javascript' src="js/jquery.easing.1.3.js"></script>
-
-	<!-- Google Analytics -->
-	<script type="text/javascript">
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-32825047-1']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	</script>
 </head>
 
 <body id='home'>
@@ -237,5 +224,22 @@
 		</div>
 	</div>
 	<script type='text/javascript' src='js/index.js'></script>
+	<script type="text/javascript">
+	  var GoSquared = {};
+	  GoSquared.acct = "GSN-647056-R";
+	  (function(w){
+	    function gs(){
+	      w._gstc_lt = +new Date;
+	      var d = document, g = d.createElement("script");
+	      g.type = "text/javascript";
+	      g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+	      var s = d.getElementsByTagName("script")[0];
+	      s.parentNode.insertBefore(g, s);
+	    }
+	    w.addEventListener ?
+	      w.addEventListener("load", gs, false) :
+	      w.attachEvent("onload", gs);
+	  })(window);
+	</script>
 </body>
 </html>
