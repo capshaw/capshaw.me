96bc3956fbca4738f5bb0790b82a76d9d8380551
Andrew Capshaw <andrew.capshaw@gmail.com>
2012-10-25 23:53:25 -0500

    Bad commiting style, but some major style changes and refactoring

--- index.php
+++ index.php
@@ -35,13 +35,6 @@
 					</div>
 				</noscript>
 				<div class='clear-both p-top-20'>
-					<div class='side-bar flowleft'>
-						<div class='outer-shell'>
-							<div class='inner-shell me'>
-								<!-- -->
-							</div>
-						</div>
-					</div>
 					<div class='main flowright'>
 						<div class='main-content'>
 							<h2>Andrew Capshaw</h2>
@@ -192,15 +185,13 @@
 			</div>
 		</div>
 		<div id='footer'>
-			<div class='container' id='contact'>
+			<div class='container'>
 				<!-- -->
 			</div>
 		</div>
 	</div>
 
-	<script type='text/javascript' src="js/jquery-1.7.2.min.js"></script>
-	<script type='text/javascript' src="js/jquery-ui-1.8.21.custom.min.js"></script>
-	<script type='text/javascript' src="js/jquery.easing.1.3.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
 	<script type='text/javascript' src='js/index.js'></script>
 
 	<!-- Google Analytics -->
