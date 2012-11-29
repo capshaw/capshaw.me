72af991decbf5de7b5b0c76ecc98669fcb445af1
capshaw <andrew.capshaw@gmail.com>
2012-11-28 23:23:57 -0600

    something

--- 2.0/index.php
+++ 2.0/index.php
@@ -28,19 +28,23 @@
 	<div class='stripe bottom-stripe transition-border'>
 		<div class='container'>
 			<p>I'm an Junior studying computer science at Rice University in Houston, TX.
-				Here are a few of my <a href='#projects' class='switcher' data-color='rgba(255,82,82, 0.8)'>projects</a>, some <a href='#code' class='switcher' data-color='rgba(136,219,125, 0.8)'>code</a> and a few ways to <a href='#contact' class='switcher' data-color='rgba(140, 179, 230, 0.8)'>contact me</a>.</p>
+				Here are a few of my <a href='#projects' class='switcher' data-color='rgba(255,82,82, 0.8)'>projects</a>,
+				some <a href='#code' class='switcher' data-color='rgba(219,112,191, 0.8)'>code</a> and a few ways to
+				<a href='#contact' class='switcher' data-color='rgba(140, 179, 230, 0.8)'>contact me</a>.
+			</p>
+		</div>
+
+		<div class='drawer hidden' id='contact'>
+			<div class='container'>by email at <a href='mailto:capshaw@rice.edu'>capshaw@rice.edu</a></div>
+		</div>
+		<div class='drawer hidden' id='projects'>
+			<div class='container'>b</div>
+		</div>
+		<div class='drawer hidden' id='code'>
+			<div class='container'>c</div>
 		</div>
 	</div>
 
-	<div class='drawer hidden' id='contact'>
-		<div class='container'></div>
-	</div>
-	<div class='drawer hidden' id='projects'>
-		<div class='container'></div>
-	</div>
-	<div class='drawer hidden' id='code'>
-		<div class='container'></div>
-	</div>
 
 	<!-- JQuery & my javascript -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
--- 2.0/js/index.js
+++ 2.0/js/index.js
@@ -1,18 +1,30 @@
+var old_name = null;
+
 /*
  * Click handlers
  */
 $(document).ready(function() {
 
+    /* Make all hidden drawers hidden for the people with JS. */
+    $('.hidden').css('display', 'none');
+
 	$('.switcher').click(function(){
 
-        var color = $(this).attr('data-color');
+        /* Don't open the same drawer twice in a row. */
+        new_name = $(this).attr('href');
+        if (new_name == old_name) return false;
+        old_name = new_name;
 
+        /* Switch accent colors. */
+        var color = $(this).attr('data-color');
 		$('h1').css('color', color);
 		$('.bottom-stripe').css('border-bottom-color', color);
 
+        /* Open the drawer. */
+        $toOpen = $(new_name);
         $('.open').hide();
-        $($(this).attr('href')).slideToggle(500);
-        $($(this).attr('href')).addClass('open');
+        ($toOpen).slideToggle(500);
+        ($toOpen).addClass('open');
 
         return false;
 	});
