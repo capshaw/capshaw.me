ee288ec2c6ff8829d99f36ab7af4330ab1afe71d
Andrew Capshaw <andrew.capshaw@gmail.com>
2013-01-25 20:05:03 -0600

    Added last.fm api on default layout

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ triggered_last_fm = false
 $(document).ready(function() {
 
     /* Load the last.fm data when the music link is clicked. */
-    // loadLastFmData();
+    loadLastFmData();
 });
 
 /**
@@ -27,7 +27,7 @@ function loadLastFmData() {
             user   : 'premendax',
             api_key: 'fdae06d5f55e33f313eec0d691b201b8'
         },
-        timeout: 3000,
+        timeout: 4000,
         success: function(data){lastFmSuccessHandler(data);},
         error: function(xhr, textStatus, errorThrown){lastFmErrorHandler(xhr, textStatus, errorThrown);},
         dataType: 'json'
@@ -39,18 +39,65 @@ function loadLastFmData() {
  */
 function lastFmSuccessHandler(data) {
 
-    // $('#last_fm_container').removeClass('hidden');
+    $('#song-list-loading').addClass('hidden');
+    $('#song-list-error').addClass('hidden');
 
     /* Iterate through the albums to append them to the music div. */
-    console.log(data);
     var tracks = data.recenttracks.track;
-    console.log(tracks);
+
+    console.log(tracks)
+
+    var now = new Date();
+    var utc = Date.UTC(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        now.getHours(),
+        now.getMinutes()
+    );
+    var tz = now.getTimezoneOffset() * 60 * 1000;
+    console.log(tz)
+
+    $('#song-list').removeClass('hidden');
     for(track_id in tracks) {
         var track = tracks[track_id];
         var url = track.url;
         var name = track.name;
+
+        var music_date = "";
+        if (track.date) {
+            var date = track.date.uts * 1000;
+            var milliseconds_since_play = utc - date + tz;
+
+            /* Find how to express track play. */
+            var a_second = 1000;
+            var a_minute = 60 * a_second;
+            var an_hour = 60 * a_minute
+            var a_day = 24 * an_hour;
+
+            var time = 1;
+            var unit = "unknown";
+            if (milliseconds_since_play > a_day) {
+                time = Math.round(milliseconds_since_play / a_day);
+                unit = "day";
+            } else if (milliseconds_since_play > an_hour) {
+                time = Math.round(milliseconds_since_play / an_hour);
+                unit = "hour";
+            } else if (milliseconds_since_play > a_minute) {
+                time = Math.round(milliseconds_since_play / a_minute);
+                unit = "minute";
+            } else {
+                time = Math.round(milliseconds_since_play / a_second);
+                unit = "second";
+            }
+            var s = time > 1 ? "s" : "";
+            music_date =  time + ' ' + unit + s + ' ago';
+        } else {
+            music_date = 'right now'
+        }
+
         var artist = track.artist['#text'];
-        var newSong = jQuery('<div/>', {
+        var newSong = jQuery('<span/>', {
             id: 'song-' + track_id,
             data_artist : artist,
             data_song_name : name
@@ -58,14 +105,22 @@ function lastFmSuccessHandler(data) {
             href : url,
             text : name
         })).append($('<span/>', {
-            text : ', ' + artist
+            text : ' by ' + artist + ' '
+        })).append($('<span/>', {
+            class : 'music-date',
+            text : music_date
         }));
 
-        newSong.appendTo($('#song_list')).fadeTo('slow', 1);
+        newSong.appendTo($('#song-list')).fadeTo('slow', 1);
+        break;
     }
 }
 
 /**
  * Let the user know that the last.fm api isn't working.
  */
-function lastFmErrorHandler(xhr, textStatus, errorThrown){ }
+function lastFmErrorHandler(xhr, textStatus, errorThrown){
+    $('#song-list-loading').addClass('hidden');
+    $('#song-list-error').removeClass('hidden');
+    console.log(errorThrown);
+}
