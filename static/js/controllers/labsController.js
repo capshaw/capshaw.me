155506d33a0609e7329c0392118e8c28bfbddfcf
Andrew Capshaw <andrew.capshaw@gmail.com>
2013-11-05 19:44:32 -0600

    Fix Firefox bug by toggling page class after inner transition finished.

--- static/js/controllers/labsController.js
+++ static/js/controllers/labsController.js
@@ -1,6 +1,22 @@
 angular.module('capshawApp')
     .controller('LabsCtrl', function ($scope, $timeout) {
+
+        $scope.labs = false;
+        $scope.pageScrollReady = false;
+        $scope.transitionTime = 400;
+
+        // Handles timing of the toggling for the page elements.
+        $scope.toggleLabs = function () {
+            $scope.labs = !$scope.labs;
+
+            $timeout(function() {
+                $scope.pageScrollReady = !$scope.pageScrollReady;
+            }, $scope.transitionTime);
+        }
+
+        // On page load, set a timeout to show the content.
         $timeout(function() {
-            $scope.labs = true;
+            $scope.toggleLabs();
         }, 200);
+
     });
