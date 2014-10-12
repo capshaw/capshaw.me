c01c0fabc26d66d0726905628d405689ad9c17a6
Andrew Capshaw <andrew.capshaw@gmail.com>
2014-10-12 09:07:31 -0500

    Fix comment style in index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
-/* Once the page has loaded, add the 'loaded' class to the body.
- * This toggles css specific to the loaded state and prevents transitions
- * from occurring as the page loads. */
+/* Once the page has loaded, add the 'loaded' class to the body. This toggles
+ * css specific to the loaded state and prevents transitions from occurring as
+ * the page loads. */
 $(document).ready(function() {
     $('body').addClass('loaded');
 });
